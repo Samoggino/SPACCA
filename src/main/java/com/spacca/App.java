49a11566725485390342636287c5c816de74923d
Simone Samoggia <simone.samoggia2@studio.unibo.it>
2024-01-12 23:43:36 +0100

    fine merge

--- src/main/java/com/spacca/App.java
+++ src/main/java/com/spacca/App.java
@@ -42,7 +42,7 @@ public class App extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/com/spacca/pages/" + fxml + ".fxml"));
         return fxmlLoader.load();
     }
-  
+
     public static void main(String[] args) {
         launch();
     }
