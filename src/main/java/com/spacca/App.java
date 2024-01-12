09384a5519a1f1b7debe4b875f08e0cc4acfe5c0
Simone Samoggia <simone.samoggia2@studio.unibo.it>
2024-01-12 23:56:41 +0100

    fix merge

--- src/main/java/com/spacca/App.java
+++ src/main/java/com/spacca/App.java
@@ -39,7 +39,7 @@ public class App extends Application {
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/com/spacca/fxml/" + fxml + ".fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/com/spacca/pages/" + fxml + ".fxml"));
         return fxmlLoader.load();
     }
 
