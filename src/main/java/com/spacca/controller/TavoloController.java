d93477b87079e6b0ea226528ba5498270ccc82e9
Simone Samoggia <simone.samoggia2@studio.unibo.it>
2024-01-17 17:21:17 +0100

    aggiunta template utenti

--- src/main/java/com/spacca/controller/TavoloController.java
+++ src/main/java/com/spacca/controller/TavoloController.java
@@ -12,13 +12,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 
 public class TavoloController implements Initializable {
 
@@ -92,7 +90,7 @@ public class TavoloController implements Initializable {
 
     }
 
-    private void buildTable() {
+    void buildTable() {
 
         try {
             for (Carta carta : partita.getCarteSulTavolo().getCarteNelMazzo()) { // prende le carte sul tavolo
@@ -114,29 +112,29 @@ public class TavoloController implements Initializable {
         // System.out.println("image dopo averla settata: " + immagineCarta.getImage());
 
         // try {
-        //     Carta cartaInCimaCarta = partita
-        //             .getPreseDellUtente(giocatore)
-        //             .getUltimaCarta();
+        // Carta cartaInCimaCarta = partita
+        // .getPreseDellUtente(giocatore)
+        // .getUltimaCarta();
 
-        //     if (cartaInCimaCarta != null) {
-        //         // immagineCarta = new ImageView("file:" + cartaInCimaCarta.getImmagine());
-        //         System.out.println("image dopo averla settata: " + immagineCarta.getImage());
-        //     }
+        // if (cartaInCimaCarta != null) {
+        // // immagineCarta = new ImageView("file:" + cartaInCimaCarta.getImmagine());
+        // System.out.println("image dopo averla settata: " + immagineCarta.getImage());
+        // }
 
-        //     Text text = new Text(giocatore);
-        //     Button myDeckButton = new Button("Le mie carte");
+        // Text text = new Text(giocatore);
+        // Button myDeckButton = new Button("Le mie carte");
 
-        //     myDeckButton.setOnAction(event -> System.out.println("\n" +
-        //             partita.getManoDellUtente(giocatore)));
+        // myDeckButton.setOnAction(event -> System.out.println("\n" +
+        // partita.getManoDellUtente(giocatore)));
 
-        //     localPane.getChildren().add(text);
-        //     localPane.getChildren().add(myDeckButton);
+        // localPane.getChildren().add(text);
+        // localPane.getChildren().add(myDeckButton);
 
-        //     containerPane.getChildren().add(localPane);
+        // containerPane.getChildren().add(localPane);
 
         // } catch (Exception e) {
-        //     System.err.println("ERRORE (showMyDeckHandler):\t\t " + e.getMessage());
-        //     e.printStackTrace();
+        // System.err.println("ERRORE (showMyDeckHandler):\t\t " + e.getMessage());
+        // e.printStackTrace();
         // }
     }
 
